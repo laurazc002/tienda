53119ad5fc76110d4359abeb7a1e51f392e20026
Laura Zuluaga <lauramariazuluaga@gmail.com>
2022-07-10 20:06:16 -0500

    se hace pagina

---
+++ src/controllers/productos.js
@@ -0,0 +1,126 @@
+let productosBD = [
+
+    {
+       nombre : 'TANK FRANÇAISE WATCH',
+       precio : '27.500.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj1.webp?alt=media&token=35ecfa1a-97e4-4105-b7f1-d9c46ab015ea'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+  {
+       nombre : 'TANK FRANÇAISE WATCH',
+       precio : '40.100.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj2...webp?alt=media&token=008d51c1-c9dd-4d71-a4bd-adb47f7e391b'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+   {
+       nombre : 'TANK LOUIS CARTIER WATCH',
+       precio : '149.500.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj3.webp?alt=media&token=480858c3-4796-4aeb-b80a-cdc20be679c3'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+   {
+       nombre : 'PANTHÈRE DE CARTIER WATCH',
+       precio : '119.000.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj4.....webp?alt=media&token=000dc8e6-931c-4502-8c31-0c8219f47dec'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+  
+   {
+       nombre : 'PANTHÈRE DE CARTIER WATCH',
+       precio : '382.000.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj5...webp?alt=media&token=42edde32-a527-487d-ae23-6cfa1b5a6e4b'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+    {
+       nombre : 'PASHA DE CARTIER WATCH',
+       precio : '603.500.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj6.webp?alt=media&token=599ba8a8-c3ab-495b-9fa0-0e6ca0a8049e'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+   {
+       nombre : 'SANTOS DE CARTIER WATCH',
+       precio : '33.900.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj7...webp?alt=media&token=8db75dbf-b3d3-4f07-a367-24f352f20e6c'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+    {
+       nombre : 'TANK MUST WATCH',
+       precio : '14.600.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj8.webp?alt=media&token=c673ef6c-4d5a-4d40-916b-62e020b25ba0'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+    {
+       nombre : 'RONDE LOUIS CARTIER WATCH',
+       precio : '175.000.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj9.webp?alt=media&token=223ceec3-40ea-46b0-b36a-01597d097e5b'],
+       descripcion : 'Reloj Cartier de mujer'
+   },
+   
+   
+    {
+       nombre : 'BALLON BLEU DE CARTIER WATCH',
+       precio : '30.200.000',
+       cantidad : '0',
+       fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj10.webp?alt=media&token=4a31a8d4-3fc5-48a6-ab43-1b2dd3194f52'],
+       descripcion : 'Reloj Cartier de mujer'
+   }
+   
+   
+   
+   
+
+
+
+]
+
+let fila = document.getElementById("fila")
+productosBD.forEach(function(producto){
+
+let columna = document.createElement("div")
+columna.classList.add("col")
+   
+let tarjeta = document.createElement("div")
+tarjeta.classList.add("card", "h-100", "text-center")
+
+let FotoProducto = document.createElement("img")
+FotoProducto.classList.add("img-fluid","w-100")
+FotoProducto.src = producto.fotos[0]
+
+let nombreProducto = document.createElement("h4")
+nombreProducto.textContent = producto.nombre
+
+let precioProducto = document.createElement("h5")
+precioProducto.classList.add("fw-bold")
+precioProducto.textContent ='$ '+ producto.precio + ' COP'
+
+columna.addEventListener("mouseover", function(){
+    FotoProducto.src = producto.fotos[1]
+   })
+
+columna.addEventListener("mouseleave", function(){
+    FotoProducto.src = producto.fotos[0]
+   })
+   
+tarjeta.appendChild(FotoProducto)
+tarjeta.appendChild(nombreProducto)
+tarjeta.appendChild(precioProducto)
+columna.appendChild(tarjeta)
+fila.appendChild(columna)
+})
+
