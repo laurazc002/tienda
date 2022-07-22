8f18e97834fd62e43c9ad93eef1b5393575d08cb
Edward <asantanadurango@gmail.com>
2022-07-22 12:23:46 -0500

    se crea carrito de compras

--- src/controllers/controladorcompra.js
+++ src/controllers/controladorcompra.js
@@ -1,2 +1,55 @@
-console.log("hola")
-console.log(localStorage.getItem("infoProducto"))
+console.log(JSON.parse(localStorage.getItem("infoProducto")))
+let producto = JSON.parse(localStorage.getItem('infoProducto'))
+
+
+let carrito
+//revisar si hay productos en el carrito
+if (JSON.parse(localStorage.getItem('carrito')) == null){
+
+    carrito = []
+
+    }
+else{
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+}
+
+console.log(JSON.parse(localStorage.getItem('carrito')))
+
+let foto = document.getElementById('foto')
+foto.src = producto.foto
+
+let nombre = document.getElementById('nombre')
+nombre.textContent = producto.nombre
+
+let precio = document.getElementById('precio')
+precio.textContent = producto.precio
+
+let descripcion = document.getElementById('descripcion')
+descripcion.textContent = producto.descripcion
+
+// capturar la cantidad del producto seleccionado
+// capturamos el value del input
+
+//let cantidad = document.getElementById('cantidad').value
+
+// agrego a un atributo (cantidad) al objeto producto
+producto.cantidad = cantidad
+//console.log(producto)
+
+// Agrego la cantidad cuando el cliente seleccione añadir carrito
+let botonagregar = document.getElementById('agregar')
+
+//agregamos carrito de compras
+
+agregar.addEventListener('click', function(){
+    let cantidad = document.getElementById('cantidad').value
+    producto.cantidad = cantidad
+
+    carrito.push(producto)
+
+    // almaceno el carrito en el localstorage
+    localStorage.setItem('carrito',JSON.stringify(carrito))
+
+
+})
+
--- src/helpers/database.js
+++ src/helpers/database.js
@@ -5,7 +5,7 @@ export let productosBD = [
        precio : '27.500.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj1.webp?alt=media&token=35ecfa1a-97e4-4105-b7f1-d9c46ab015ea','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj1...webp?alt=media&token=188e2720-1cf6-480e-b10d-086b4ed1e71e'],
-       descripcion : 'Reloj Cartier de mujer,tamaño mediano, movimiento de cuarzo. Caja de acero, corona octogonal de acero decorada con un cabujón de espinela sintética. Esfera graneada plateada. Agujas de acero azulado en forma de espada. Cristal de zafiro. Brazalete de acero. Dimensiones de la caja: 30 mm x 25 mm. Grosor: 6,65 mm. Hermético hasta 3 bares (~30 metros).'
+       descripcion : 'Reloj tamaño mediano, movimiento de cuarzo. Caja de acero, corona octogonal de acero decorada con un cabujón de espinela sintética. Esfera graneada plateada. Agujas de acero azulado en forma de espada. Cristal de zafiro. Brazalete de acero. Dimensiones de la caja: 30 mm x 25 mm. Grosor: 6,65 mm. Hermético hasta 3 bares (~30 metros).'
    },
    
   {
@@ -13,7 +13,7 @@ export let productosBD = [
        precio : '40.100.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj2....webp?alt=media&token=ac83e308-c47c-4df9-bcfa-62ae88846b88','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj2...webp?alt=media&token=008d51c1-c9dd-4d71-a4bd-adb47f7e391b'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj tamaño mediano, movimiento de cuarzo. Caja de acero engastada con 26 diamantes talla brillante con un total de 0,63 quilates. Corona poliédrica decorada con una espinela sintética. Esfera plateada. Agujas de acero azulado en forma de espada. Cristal de zafiro. Brazalete de acero. Dimensiones de la caja: 30 mm x 25 mm. Grosor: 6,65 mm. Hermético hasta 3 bares (~30 metros).'
    },
    
    {
@@ -21,7 +21,7 @@ export let productosBD = [
        precio : '149.500.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj3.webp?alt=media&token=480858c3-4796-4aeb-b80a-cdc20be679c3','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj3...webp?alt=media&token=cc087e3a-17ba-4564-8a8f-d7ff73ddf0d8'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj tamaño grande, movimiento mecánico Manufactura de cuerda manual calibre 1917 MC. Caja y corona perlada de oro rosa 750/1000 engastadas con 41 diamantes talla brillante con un total de 0,66 quilates. Esfera plateada. Agujas de acero azulado en forma de espada. Cristal mineral. Brazalete de oro rosa 750/1000. Dimensiones de la caja: 33,7 mm x 25,5 mm. Grosor: 6,6 mm. Hermético hasta 3 bares (~30 metros).'
    },
    
    {
@@ -29,7 +29,7 @@ export let productosBD = [
        precio : '119.000.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj4.....webp?alt=media&token=000dc8e6-931c-4502-8c31-0c8219f47dec','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj4...webp?alt=media&token=6c6bbd6b-93f2-4fb3-ba62-aa85f07c8fd2'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj tamaño pequeño, movimiento de cuarzo. Caja de acero. Dimensiones: 22 mm x 30 mm. Grosor: 6 mm. Corona decorada con una espinela sintética azul. Esfera con degradado del negro al gris que forma una diagonal en el centro. Agujas en forma de espada. Brazalete de acero. Hermético hasta 3 bares (~30 metros).'
    },
   
    {
@@ -37,7 +37,7 @@ export let productosBD = [
        precio : '382.000.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj5...webp?alt=media&token=42edde32-a527-487d-ae23-6cfa1b5a6e4b','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj5....webp?alt=media&token=2843f819-c5ab-4b9d-b67f-3443c6318b16'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier tamaño pequeño, movimiento de cuarzo. Caja, corona y brazalete de oro rosa 750/1000 engastados con 497 diamantes talla brillante con un total de 4,18 quilates. Agujas de acero azulado en forma de espada. Dimensiones: 30 mm x 22 mm. Grosor: 6 mm. Hermético hasta 3 bares (~30 metros).'
    },
    
     {
@@ -45,7 +45,7 @@ export let productosBD = [
        precio : '603.500.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj6.webp?alt=media&token=599ba8a8-c3ab-495b-9fa0-0e6ca0a8049e','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj6...webp?alt=media&token=645fea2d-7bd8-4cba-ba04-06b29d777370'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier 35 mm, movimiento mecánico de carga automática calibre 1847 MC. Caja de oro blanco rodiado 750/1000. Bisel, brazalete y cubierta de corona engastados con 488 diamantes talla brillante con un total de 8,78 quilates. Corona adornada con un zafiro. Esfera flinqué plateada. Agujas de acero azulado en forma de rombo. Brazalete de oro blanco rodiado 750/1000. Diámetro de la caja: 35 mm. Grosor: 9,37 mm. Hermético hasta 10 bares (~ 100 metros).'
    },
    
    {
@@ -53,7 +53,7 @@ export let productosBD = [
        precio : '33.900.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj7.webp?alt=media&token=6c5a145f-00ae-43d1-a3c3-262686ba67d6','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj7...webp?alt=media&token=8db75dbf-b3d3-4f07-a367-24f352f20e6c'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier tamaño grande, movimiento mecánico de carga automática calibre 1847 MC. Caja de acero. Corona heptagonal decorada con una espinela sintética facetada. Esfera azul degradado. Agujas de acero en forma de espada. Cristal de zafiro. Brazalete de acero con sistema de modificación de la talla SmartLink. Correa adicional de piel de becerro color azul marino, con hebilla desplegable intercambiable de acero. Tanto el brazalete como la correa están equipados con el sistema intercambiable QuickSwitch. Ancho de la caja: 39,8 mm. Grosor: 9,08 mm. Hermético hasta 10 bares (~100 metros).'
    },
    
     {
@@ -61,7 +61,7 @@ export let productosBD = [
        precio : '14.600.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj8.webp?alt=media&token=c673ef6c-4d5a-4d40-916b-62e020b25ba0','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj8...webp?alt=media&token=bfd199c7-c438-4276-8bb2-878b96b70470'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier tamaño XL, movimiento mecánico de carga automática calibre 1847 MC. Caja de acero. Corona perlada decorada con un cabujón de espinela sintética. Esfera flinqué plateada. Agujas de acero azulado en forma de espada. Brazalete de acero. Apertura de calendario a las 6 horas. Dimensiones de la caja: 41 mm x 31 mm. Grosor: 8,4 mm. Hermético hasta 3 bares (~30 metros).'
    },
    
     {
@@ -69,7 +69,7 @@ export let productosBD = [
        precio : '175.000.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj9.webp?alt=media&token=223ceec3-40ea-46b0-b36a-01597d097e5b','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj9.....webp?alt=media&token=3dfcf684-894e-4cd7-aebc-21b5f9868bea'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier 40 mm, movimiento mecánico Manufactura de carga automática calibre 1904-PS MC, caja plateada 750/1000, corona perlada decorada con un cabujón de zafiro, esfera flinqué plateada, números romanos y árabes, manecillas en forma de pomo de acero azulado, cristal zafiro, correa de piel de aligátor negra, hebilla plateada 750/1000, segundero pequeño a las 6 y fecha a las 3, 9,2 mm de grosor, hermético hasta 3 bares (30 metros).'
    },
    
    
@@ -78,7 +78,7 @@ export let productosBD = [
        precio : '30.200.000',
        cantidad : '0',
        fotos : ['https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj10.webp?alt=media&token=4a31a8d4-3fc5-48a6-ab43-1b2dd3194f52','https://firebasestorage.googleapis.com/v0/b/tiendatcc-bccd5.appspot.com/o/reloj10...webp?alt=media&token=c81b7d27-df20-47c3-af87-46c193ef5068'],
-       descripcion : 'Reloj Cartier de mujer'
+       descripcion : 'Reloj Cartier 40 mm, movimiento mecánico de carga automática calibre 1847 MC. Caja de acero. Corona acanalada decorada con un cabujón de espinela sintética. Esfera color gris oscuro efecto rayos de sol. Agujas plateadas en forma de espada. Cristal de zafiro. Brazalete intercambiable de acero. Apertura de calendario a las 3 horas. Grosor: 12,4 mm. Hermético hasta 3 bares (~30 metros).'
    }
    
 
