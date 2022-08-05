e0f23cdea7b4b84f8dc2d2773ec2015a94c439d3
Laura Zuluaga <lauramariazuluaga@gmail.com>
2022-08-04 20:36:47 -0500

    se crea convertidor de moneda

---
+++ src/controllers/convertirmoneda.js
@@ -0,0 +1,6 @@
+function dolaresPesos(valNum) {
+    document.getElementById("inputPesos").value=valNum*4.306;
+  }
+  function pesosDolares(valNum) {
+    document.getElementById("inputDolares").value=valNum*0.00023;
+  }
